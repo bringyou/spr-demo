d7e7e53cb6a1c341f2f0167f2894363b3647a189
YuBinglei <yubinglei2012@foxmail.com>
2025-02-13 18:40:41 +0800

    first commit

---
+++ src/main/java/com/github/by/sprlifecycledemo/Demo.java
@@ -0,0 +1,4 @@
+package com.github.by.sprlifecycledemo;
+
+public class Demo {
+}
---
+++ src/main/java/com/github/by/sprlifecycledemo/SprLifecycleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.by.sprlifecycledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprLifecycleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprLifecycleDemoApplication.class, args);
+	}
+
+}
