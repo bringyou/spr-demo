5eaf2a1996785deada1be35440e59e4b7d55fc4d
YuBinglei <yubinglei2012@foxmail.com>
2025-02-13 18:40:57 +0800

    init

--- src/main/java/com/github/by/sprlifecycledemo/Demo.java
+++ src/main/java/com/github/by/sprlifecycledemo/Demo.java
@@ -1,4 +1,21 @@
 package com.github.by.sprlifecycledemo;
 
-public class Demo {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Demo implements DisposableBean {
+    private final RedisConnectionFactory connectionFactory;
+
+    public Demo(RedisConnectionFactory connectionFactory) {
+        this.connectionFactory = connectionFactory;
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("begin destroy");
+        String resp = connectionFactory.getConnection().ping();
+    }
 }
--- src/main/java/com/github/by/sprlifecycledemo/SprLifecycleDemoApplication.java
+++ src/main/java/com/github/by/sprlifecycledemo/SprLifecycleDemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SprLifecycleDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SprLifecycleDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SprLifecycleDemoApplication.class, args);
+    }
 
 }
